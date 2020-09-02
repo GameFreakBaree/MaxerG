06a256afea89c8c5e6a38b0a1366e095963aa93b
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-09-02 16:34:26 +0200

    Update automoderator.py

--- moderator/automoderator.py
+++ moderator/automoderator.py
@@ -7,31 +7,17 @@ from discord.utils import get
 import datetime
 import mysql.connector
 
-with open('./db_settings.json', 'r', encoding='utf-8') as read_settings:
+with open('./config.json', 'r', encoding='utf-8') as read_settings:
     settings = json.load(read_settings)
 
 host = settings['host']
 user = settings['user']
 password = settings['password']
 database = settings['database']
-
+embedcolor = settings['embedcolor']
+embed_footer = settings['footer']
 read_settings.close()
-
-db_maxerg = mysql.connector.connect(
-    host=host,
-    database=database,
-    user=user,
-    passwd=password
-)
-
-maxergdb_cursor = db_maxerg.cursor()
-
-maxergdb_cursor.execute("SELECT embedcolor FROM maxerg_config")
-embed_color_tuple = maxergdb_cursor.fetchone()
-embed_color = int(embed_color_tuple[0], 16)
-
-maxergdb_cursor.execute("SELECT footer FROM maxerg_config")
-embed_footer = maxergdb_cursor.fetchone()
+embed_color = int(embedcolor, 16)
 
 
 class AutoModerator(commands.Cog):
@@ -45,16 +31,16 @@ class AutoModerator(commands.Cog):
         if member is None:
             member = ctx.author
 
-        db_maxerg.commit()
-        mod_maxergdb_cursor = db_maxerg.cursor()
+        db_maxerg = mysql.connector.connect(host=host, database=database, user=user, passwd=password)
+        maxergdb_cursor = db_maxerg.cursor()
 
-        mod_maxergdb_cursor.execute(f"SELECT count FROM maxerg_modlogs WHERE user_id = {member.id}")
-        interfraction_tuple = mod_maxergdb_cursor.fetchone()
+        maxergdb_cursor.execute(f"SELECT count FROM maxerg_modlogs WHERE user_id = {member.id}")
+        interfraction_tuple = maxergdb_cursor.fetchone()
 
         if interfraction_tuple is None:
             sql_insert_modlogs = "INSERT INTO maxerg_modlogs (user_id, count) VALUES (%s, %s)"
             record = (f"{member.id}", 0)
-            mod_maxergdb_cursor.execute(sql_insert_modlogs, record)
+            maxergdb_cursor.execute(sql_insert_modlogs, record)
             db_maxerg.commit()
 
             interfraction = 0
@@ -74,8 +60,9 @@ class AutoModerator(commands.Cog):
         )
         embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
         embed.set_thumbnail(url=member.avatar_url)
-        embed.set_footer(text=embed_footer[0])
+        embed.set_footer(text=embed_footer)
         await ctx.send(embed=embed)
+        db_maxerg.close()
 
     @interfractions.error
     async def interfractions_error(self, ctx, error):
@@ -93,102 +80,107 @@ class AutoModerator(commands.Cog):
         if str(message.channel) not in staff_channels:
             admin_role = get(message.guild.roles, name="🔥│Sr. Moderator")
             if admin_role not in message.author.roles:
-                for link in verboden_links:
-                    if message.content.count(link) > 0:
-                        await message.channel.purge(limit=1)
-                        counter_message2 = await message.channel.send(
-                            f"HEY! Deze link is verboden, {message.author.mention}!")
+                db_maxerg = mysql.connector.connect(host=host, database=database, user=user, passwd=password)
+                mod_maxergdb_cursor = db_maxerg.cursor()
 
-                        log_channel = self.client.get_channel(742715965128704030)
-                        embed = discord.Embed(
-                            color=embed_color,
-                            timestamp=datetime.datetime.utcnow()
-                        )
-                        embed.add_field(name="Gebruiker",
-                                        value=f"**Naam:** {message.author.mention}\n**ID:** {message.author.id}",
-                                        inline=False)
-                        embed.add_field(name="Bericht", value=f"{message.content}", inline=False)
-                        embed.set_author(name=f"[AUTOMODERATIE] {message.author}", icon_url=message.author.avatar_url)
-                        embed.set_thumbnail(url=message.author.avatar_url)
-                        embed.set_footer(text=embed_footer[0])
-                        await log_channel.send(embed=embed)
+                mod_maxergdb_cursor.execute(
+                    f"SELECT ticket_channel_ids FROM maxerg_tickets WHERE ticket_member_ids = {message.author.id} AND ticket_channel_ids = {message.channel.id}")
+                channel_id_list = mod_maxergdb_cursor.fetchone()
 
-                        db_maxerg.commit()
-                        mod_maxergdb_cursor = db_maxerg.cursor()
+                if channel_id_list is not None and message.channel.id != channel_id_list[0]:
+                    for link in verboden_links:
+                        if message.content.count(link) > 0:
+                            await message.channel.purge(limit=1)
+                            counter_message2 = await message.channel.send(
+                                f"HEY! Deze link is verboden, {message.author.mention}!")
 
-                        mod_maxergdb_cursor.execute(f"SELECT count FROM maxerg_modlogs WHERE user_id = {message.author.id}")
-                        interfraction_tuple = mod_maxergdb_cursor.fetchone()
+                            log_channel = self.client.get_channel(561243076450975754)
+                            embed = discord.Embed(
+                                color=embed_color,
+                                timestamp=datetime.datetime.utcnow()
+                            )
+                            embed.add_field(name="Gebruiker",
+                                            value=f"**Naam:** {message.author.mention}\n**ID:** {message.author.id}",
+                                            inline=False)
+                            embed.add_field(name="Bericht", value=f"{message.content}", inline=False)
+                            embed.set_author(name=f"[AUTOMODERATIE] {message.author}", icon_url=message.author.avatar_url)
+                            embed.set_thumbnail(url=message.author.avatar_url)
+                            embed.set_footer(text=embed_footer)
+                            await log_channel.send(embed=embed)
 
-                        if interfraction_tuple is None:
-                            sql_insert_modlogs = "INSERT INTO maxerg_modlogs (user_id, count) VALUES (%s, %s)"
-                            record = (f"{message.author.id}", 1)
-                            mod_maxergdb_cursor.execute(sql_insert_modlogs, record)
-                            db_maxerg.commit()
-                        else:
-                            new_count = interfraction_tuple[0] + 1
-                            sql_update_modlogs = f"UPDATE maxerg_modlogs SET count = {new_count} WHERE user_id = {message.author.id}"
-                            mod_maxergdb_cursor.execute(sql_update_modlogs)
-                            db_maxerg.commit()
+                            mod_maxergdb_cursor.execute(f"SELECT count FROM maxerg_modlogs WHERE user_id = {message.author.id}")
+                            interfraction_tuple = mod_maxergdb_cursor.fetchone()
 
-                            if new_count % 5 == 0:
-                                role_check = discord.utils.find(lambda r: r.name == 'Muted', message.guild.roles)
-                                if role_check not in message.author.roles:
-                                    role = get(message.guild.roles, name="Muted")
-                                    await message.author.add_roles(role)
+                            if interfraction_tuple is None:
+                                sql_insert_modlogs = "INSERT INTO maxerg_modlogs (user_id, count) VALUES (%s, %s)"
+                                record = (f"{message.author.id}", 1)
+                                mod_maxergdb_cursor.execute(sql_insert_modlogs, record)
+                                db_maxerg.commit()
+                            else:
+                                new_count = interfraction_tuple[0] + 1
+                                sql_update_modlogs = f"UPDATE maxerg_modlogs SET count = {new_count} WHERE user_id = {message.author.id}"
+                                mod_maxergdb_cursor.execute(sql_update_modlogs)
+                                db_maxerg.commit()
 
-                                    premute_embed = discord.Embed(
-                                        description=f"**Reden:** Automoderatie\n**Duratie:** 24u",
-                                        color=embed_color
-                                    )
-                                    premute_embed.set_author(name=f"{message.author} is gemute!",
-                                                             icon_url=message.author.avatar_url)
-                                    await message.channel.send(embed=premute_embed)
-
-                                    log_channel = self.client.get_channel(742715965128704030)
-
-                                    mute_embed = discord.Embed(
-                                        color=embed_color,
-                                        timestamp=datetime.datetime.utcnow()
-                                    )
-                                    mute_embed.add_field(name="Gebruiker",
-                                                         value=f"**Naam:**\t{message.author.mention}\n**ID:**\t{message.author.id}",
-                                                         inline=True)
-                                    mute_embed.add_field(name="Moderator",
-                                                         value=f"**Naam:**\t{self.client.user.mention}\n**ID:**\t{self.client.user.id}",
-                                                         inline=True)
-                                    mute_embed.add_field(name="Reden", value=f"Automoderatie", inline=False)
-                                    mute_embed.add_field(name="Duur", value="24 uur", inline=False)
-                                    mute_embed.set_author(name=f"[TEMPMUTE] {message.author}",
-                                                          icon_url=message.author.avatar_url)
-                                    mute_embed.set_footer(text=embed_footer[0])
-                                    await log_channel.send(embed=mute_embed)
-
-                                    await asyncio.sleep(86400)
-
+                                if new_count % 5 == 0:
                                     role_check = discord.utils.find(lambda r: r.name == 'Muted', message.guild.roles)
-                                    if role_check in message.author.roles:
-                                        role = discord.utils.get(message.guild.roles, name="Muted")
-                                        await message.author.remove_roles(role)
+                                    if role_check not in message.author.roles:
+                                        role = get(message.guild.roles, name="Muted")
+                                        await message.author.add_roles(role)
 
-                                        log_channel = self.client.get_channel(742715965128704030)
+                                        premute_embed = discord.Embed(
+                                            description=f"**Reden:** Automoderatie\n**Duratie:** 24u",
+                                            color=embed_color
+                                        )
+                                        premute_embed.set_author(name=f"{message.author} is gemute!",
+                                                                 icon_url=message.author.avatar_url)
+                                        await message.channel.send(embed=premute_embed)
 
-                                        unmute_embed = discord.Embed(
+                                        log_channel = self.client.get_channel(561243076450975754)
+
+                                        mute_embed = discord.Embed(
                                             color=embed_color,
                                             timestamp=datetime.datetime.utcnow()
                                         )
-                                        unmute_embed.add_field(name="Gebruiker",
-                                                               value=f"**Naam:**\t{message.author.mention}\n**ID:**\t{message.author.id}",
-                                                               inline=True)
-                                        unmute_embed.add_field(name="Moderator",
-                                                               value=f"**Naam:**\t{self.client.user.mention}\n**ID:**\t{self.client.user.id}",
-                                                               inline=True)
-                                        unmute_embed.set_author(name=f"[UNMUTE] {message.author}",
-                                                                icon_url=message.author.avatar_url)
-                                        unmute_embed.set_footer(text=embed_footer[0])
-                                        await log_channel.send(embed=unmute_embed)
+                                        mute_embed.add_field(name="Gebruiker",
+                                                             value=f"**Naam:**\t{message.author.mention}\n**ID:**\t{message.author.id}",
+                                                             inline=True)
+                                        mute_embed.add_field(name="Moderator",
+                                                             value=f"**Naam:**\t{self.client.user.mention}\n**ID:**\t{self.client.user.id}",
+                                                             inline=True)
+                                        mute_embed.add_field(name="Reden", value=f"Automoderatie", inline=False)
+                                        mute_embed.add_field(name="Duur", value="24 uur", inline=False)
+                                        mute_embed.set_author(name=f"[TEMPMUTE] {message.author}",
+                                                              icon_url=message.author.avatar_url)
+                                        mute_embed.set_footer(text=embed_footer[0])
+                                        await log_channel.send(embed=mute_embed)
 
-                        await asyncio.sleep(3)
-                        await counter_message2.delete()
+                                        await asyncio.sleep(86400)
+
+                                        role_check = discord.utils.find(lambda r: r.name == 'Muted', message.guild.roles)
+                                        if role_check in message.author.roles:
+                                            role = discord.utils.get(message.guild.roles, name="Muted")
+                                            await message.author.remove_roles(role)
+
+                                            log_channel = self.client.get_channel(561243076450975754)
+
+                                            unmute_embed = discord.Embed(
+                                                color=embed_color,
+                                                timestamp=datetime.datetime.utcnow()
+                                            )
+                                            unmute_embed.add_field(name="Gebruiker",
+                                                                   value=f"**Naam:**\t{message.author.mention}\n**ID:**\t{message.author.id}",
+                                                                   inline=True)
+                                            unmute_embed.add_field(name="Moderator",
+                                                                   value=f"**Naam:**\t{self.client.user.mention}\n**ID:**\t{self.client.user.id}",
+                                                                   inline=True)
+                                            unmute_embed.set_author(name=f"[UNMUTE] {message.author}",
+                                                                    icon_url=message.author.avatar_url)
+                                            unmute_embed.set_footer(text=embed_footer[0])
+                                            await log_channel.send(embed=unmute_embed)
+                            await asyncio.sleep(3)
+                            await counter_message2.delete()
+                db_maxerg.close()
 
 
 def setup(client):
