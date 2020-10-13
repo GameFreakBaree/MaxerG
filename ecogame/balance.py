fef0f7213c1b0e4b608abda4877db19a19268984
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-13 17:25:20 +0200

    Update balance.py

--- ecogame/balance.py
+++ ecogame/balance.py
@@ -1,22 +1,8 @@
 import discord
 from discord.ext import commands
-import json
 import datetime
 import mysql.connector
-
-with open('./config.json', 'r', encoding='utf-8') as read_settings:
-    settings = json.load(read_settings)
-
-host = settings['host']
-user = settings['user']
-password = settings['password']
-database = settings['database']
-currency = settings['currency']
-embedcolor = settings['embedcolor']
-embed_footer = settings['footer']
-read_settings.close()
-
-embed_color = int(embedcolor, 16)
+from settings import host, user, password, database, embedcolor, footer, currency, ecogame_channels
 
 
 class EcoBalance(commands.Cog):
@@ -25,8 +11,8 @@ class EcoBalance(commands.Cog):
         self.client = client
 
     @commands.command(aliases=["bal"])
+    @commands.cooldown(1, 5, commands.BucketType.user)
     async def balance(self, ctx, *, member: discord.Member = None):
-        ecogame_channels = ["💰│economy-game", "🔒│bots", "🔒│staff"]
         if str(ctx.channel) in ecogame_channels:
             if member is None:
                 member = ctx.author
@@ -34,29 +20,45 @@ class EcoBalance(commands.Cog):
             db_maxerg = mysql.connector.connect(host=host, database=database, user=user, passwd=password)
             maxergdb_cursor = db_maxerg.cursor()
 
-            maxergdb_cursor.execute(f"SELECT cash FROM maxerg_ecogame WHERE user_id = {member.id}")
-            cash = maxergdb_cursor.fetchone()
-            if cash is None:
-                cash = (0,)
+            maxergdb_cursor.execute(f"SELECT * FROM maxerg_economie WHERE user_id = {member.id}")
+            eco_data = maxergdb_cursor.fetchone()
+            cash = eco_data[1]
+            bank = eco_data[2]
+            netto = eco_data[3]
+            max_bank = eco_data[8]
 
-            maxergdb_cursor.execute(f"SELECT bank FROM maxerg_ecogame WHERE user_id = {member.id}")
-            bank = maxergdb_cursor.fetchone()
-            if bank is None:
-                bank = (0,)
+            if cash is None or bank is None or max_bank is None:
+                cash = 0
+                bank = 0
+                max_bank = 5000
 
-            net_worth = cash[0] + bank[0]
+            db_maxerg.close()
 
             em = discord.Embed(
-                color=embed_color,
+                color=embedcolor,
                 timestamp=datetime.datetime.utcnow()
             )
-            em.add_field(name="Cash:", value=f"{currency}{cash[0]}", inline=True)
-            em.add_field(name="Bank:", value=f"{currency}{bank[0]}", inline=True)
-            em.add_field(name="Net Worth:", value=f"{currency}{net_worth}", inline=True)
+            em.add_field(name="Cash:", value=f"{currency}{cash}", inline=True)
+            em.add_field(name="Bank:", value=f"{currency}{bank}/{max_bank}", inline=True)
+            em.add_field(name="Totaal:", value=f"{currency}{netto}", inline=True)
             em.set_author(name=f"{member}", icon_url=f"{member.avatar_url}")
-            em.set_footer(text=embed_footer)
+            em.set_footer(text=footer)
             await ctx.send(embed=em)
-            db_maxerg.close()
+        else:
+            await ctx.send("Deze command werkt alleen in <#708055327958106164>.")
+            ctx.command.reset_cooldown(ctx)
+
+    @balance.error
+    async def balance_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            em = discord.Embed(
+                description=f"<:error:725030739531268187> Je moet {round(error.retry_after, 1)} seconden wachten om deze command opnieuw te gebruiken.",
+                color=embedcolor,
+                timestamp=datetime.datetime.utcnow()
+            )
+            em.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
+            em.set_footer(text=footer)
+            await ctx.send(embed=em)
 
 
 def setup(client):
