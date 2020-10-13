1f85add511e9bfc922eb620a6595c861e8b4e8fe
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-13 17:16:12 +0200

    Update kick.py

--- moderator/kick.py
+++ moderator/kick.py
@@ -1,20 +1,7 @@
 import discord
 import datetime
-import json
 from discord.ext import commands
-from discord.ext.commands import MissingPermissions
-
-with open('./config.json', 'r', encoding='utf-8') as read_settings:
-    settings = json.load(read_settings)
-
-host = settings['host']
-user = settings['user']
-password = settings['password']
-database = settings['database']
-embedcolor = settings['embedcolor']
-embed_footer = settings['footer']
-read_settings.close()
-embed_color = int(embedcolor, 16)
+from settings import footer, embedcolor
 
 
 class Kick(commands.Cog):
@@ -32,14 +19,14 @@ class Kick(commands.Cog):
 
             prekick_embed = discord.Embed(
                 description=f"**Reden:** {reason}",
-                color=embed_color
+                color=embedcolor
             )
             prekick_embed.set_author(name=f"{member} is gekicked!", icon_url=member.avatar_url)
             await ctx.send(embed=prekick_embed)
 
             log_channel = self.client.get_channel(561243076450975754)
             kick_embed = discord.Embed(
-                color=embed_color,
+                color=embedcolor,
                 timestamp=datetime.datetime.utcnow()
             )
             kick_embed.add_field(name="Gebruiker", value=f"**Naam:**\t{member.mention}\n**ID:**\t{member.id}",
@@ -48,13 +35,13 @@ class Kick(commands.Cog):
                                  value=f"**Naam:**\t{ctx.author.mention}\n**ID:**\t{ctx.author.id}", inline=True)
             kick_embed.add_field(name="Reden", value=f"{reason}", inline=False)
             kick_embed.set_author(name=f"[KICK] {member}", icon_url=member.avatar_url)
-            kick_embed.set_footer(text=embed_footer)
+            kick_embed.set_footer(text=footer)
             await log_channel.send(embed=kick_embed)
 
     @kick.error
     async def kick_error(self, ctx, error):
-        if isinstance(error, MissingPermissions):
-            return
+        if isinstance(error, commands.MissingPermissions):
+            pass
 
 
 def setup(client):
