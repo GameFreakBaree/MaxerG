c8ca1738984d3c916fad8dc187c13fc1ff8624d4
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-13 17:29:09 +0200

    Update slut.py

--- ecogame/slut.py
+++ ecogame/slut.py
@@ -1,25 +1,11 @@
 import discord
 from discord.ext import commands
-import json
 import random
 from random import randint
 import time
 import datetime
 import mysql.connector
-
-with open('./config.json', 'r', encoding='utf-8') as read_settings:
-    settings = json.load(read_settings)
-
-host = settings['host']
-user = settings['user']
-password = settings['password']
-database = settings['database']
-currency = settings['currency']
-embedcolor = settings['embedcolor']
-embed_footer = settings['footer']
-read_settings.close()
-
-embed_color = int(embedcolor, 16)
+from settings import host, user, password, database, embedcolor, footer, currency, ecogame_channels
 
 
 class EcoSlut(commands.Cog):
@@ -28,43 +14,94 @@ class EcoSlut(commands.Cog):
         self.client = client
 
     @commands.command()
-    @commands.cooldown(1, 7200, commands.BucketType.user)
+    @commands.cooldown(1, 10800, commands.BucketType.user)
     async def slut(self, ctx):
-        minigame_channels = ["💰│economy-game", "🔒│bots"]
-        if str(ctx.channel) in minigame_channels:
+        if str(ctx.channel) in ecogame_channels:
             db_maxerg = mysql.connector.connect(host=host, database=database, user=user, passwd=password)
             maxergdb_cursor = db_maxerg.cursor()
+            
+            failrate = randint(1, 5)
 
-            failrate = randint(0, 3)
-            if failrate == 1:
-                loon = randint(20, 50)
-                loon_cast = int(loon)
+            if failrate == 3:
+                loon = randint(120, 250)
+                loon = int(loon)
 
-                maxergdb_cursor.execute(f"UPDATE maxerg_ecogame SET cash = cash - {loon_cast} WHERE user_id = {ctx.author.id}")
-                maxergdb_cursor.execute(f"UPDATE maxerg_ecogame SET netto = netto - {loon_cast} WHERE user_id = {ctx.author.id}")
+                maxergdb_cursor.execute(f"UPDATE maxerg_economie SET cash = cash - {loon} WHERE user_id = {ctx.author.id}")
+                maxergdb_cursor.execute(f"UPDATE maxerg_economie SET netto = netto - {loon} WHERE user_id = {ctx.author.id}")
                 db_maxerg.commit()
 
+                risico_new = randint(1, 8)
+                maxergdb_cursor.execute(f"SELECT risico FROM maxerg_economie WHERE user_id = {ctx.author.id}")
+                eco_data = maxergdb_cursor.fetchone()
+                risico = eco_data[0] + risico_new
+
+                risico_waarde = randint(1, 100)
+                if risico_waarde <= risico or risico >= 91:
+                    current_time = datetime.datetime.utcnow()
+                    last_work = current_time.strftime("%Y-%m-%d %H:%M:%S")
+                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET risico = %s WHERE user_id = %s", (104, ctx.author.id))
+                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET job = %s WHERE user_id = %s", ("werkloos", ctx.author.id))
+                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET last_work = %s WHERE user_id = %s", (last_work, ctx.author.id))
+                    db_maxerg.commit()
+
+                    em = discord.Embed(
+                        description=f"Je nam een risico om dit te doen met je job. Je bent Ontslagen!\nJe hebt een cooldown van `7 dagen` om terug te solliciteren.",
+                        color=embedcolor,
+                        timestamp=datetime.datetime.utcnow()
+                    )
+                    em.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
+                    em.set_footer(text=footer)
+                    await ctx.send(embed=em)
+                else:
+                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET risico = {risico} WHERE user_id = {ctx.author.id}")
+                    db_maxerg.commit()
+
+                # v3.2 - 5 Antwoorden
+                # v4.0 - 15 Antwoorden
                 mogelijke_antwoorden = [
-                    f"De politie viel je huis binnen en je kreeg een boete van {currency}{loon_cast}.",
-                    f"Een klant viel je aan en heeft {currency}{loon_cast} genomen en is weggelopen.",
-                    f"Je bent betrapt toen je een leuke tijd had in de badkamer en kreeg een boete van {currency}{loon_cast}.",
-                    f"Je vibrator viel in het midden van je show uit, je klant wou een stuk van zijn geld terug, dus je hebt hem {currency}{loon_cast} betaald.",
-                    f"Je was naakt aan het wandelen op de straat en kreeg een boete van {currency}{loon_cast} voor het verstoren van de openbare orde."]
+                    # 0-5
+                    f"De politie viel je huis binnen en je kreeg een boete van {currency}{loon}.",
+                    f"Een klant viel je aan en heeft {currency}{loon} genomen en is weggelopen.",
+                    f"Je bent betrapt toen je een leuke tijd had in de badkamer en kreeg een boete van {currency}{loon}.",
+                    f"Je vibrator viel in het midden van je show uit, je klant wou een stuk van zijn geld terug, dus je hebt hem {currency}{loon} betaald.",
+                    f"Je was naakt aan het wandelen op de straat en kreeg een boete van {currency}{loon} voor het verstoren van de openbare orde.",
+                    # 6-10
+                    f"Je bent opgepakt omdat je seks in het openbaar had, je kreeg een boete van {currency}{loon}.",
+                    f"Je bent betrapt door in de bib, je betaalt een boete van {currency}{loon}.",
+                    f"Je probeerde een sexy standje maar je slipte en brak je been, je betaalde {currency}{loon} aan het ziekenhuis.",
+                    f"Terwijl je aan het werken was op de straat voelde je jezelf niet goed. Je ging naar de dokter en hij rekende je {currency}{loon} aan.",
+                    f"Je bent betrapt terwijl je seks had in iemand zijn auto en betaalde {currency}{loon} aan de eigenaar van die auto.",
+                    # 11 - 15
+                    f"Je gebruikte protectie maar het veroorzaakte een allergische reactie en betaalde {currency}{loon} aan de dokter.",
+                    f"Je ging naar beneden bij een boekhouder, de kosten van je belastingen zijn {currency}{loon} verhoogd.",
+                    f"Hij wou niet omhoog blijven en je verloor {currency}{loon} aan een klant.",
+                    f"Je nam je eigen video op en zette deze op PH. Je baas kwam erachter en je moest {currency}{loon} geven om je job te behouden.",
+                    f"Het bed van het hotel hangt vol witte plekken. Je moest {currency}{loon} betalen om het te wassen."]
                 antwoord = random.choice(mogelijke_antwoorden)
                 color_succes_fail = 0xFF0000
             else:
-                loon = randint(30, 60)
-                loon_cast = int(loon)
+                loon = randint(180, 380)
+                loon = int(loon)
 
-                maxergdb_cursor.execute(f"UPDATE maxerg_ecogame SET cash = cash + {loon_cast} WHERE user_id = {ctx.author.id}")
-                maxergdb_cursor.execute(f"UPDATE maxerg_ecogame SET netto = netto + {loon_cast} WHERE user_id = {ctx.author.id}")
+                maxergdb_cursor.execute(f"UPDATE maxerg_economie SET cash = cash + {loon} WHERE user_id = {ctx.author.id}")
+                maxergdb_cursor.execute(f"UPDATE maxerg_economie SET netto = netto + {loon} WHERE user_id = {ctx.author.id}")
                 db_maxerg.commit()
 
-                mogelijke_antwoorden = [f"Je kreeg {currency}{loon_cast} om te dansen in een stripclub.",
-                                        f"Je Suger Daddy betaalde je {currency}{loon_cast} voor het werk dat je deed voor hem vorige nacht.",
-                                        f"Je bent heel goed met je handen en kreeg {currency}{loon_cast}.",
-                                        f"Je hebt {currency}{loon_cast} verdient om naaktfoto's online te verkopen.",
-                                        f"Je hebt een Onlyfans gestart en verdiende {currency}{loon_cast} op de eerste dag."]
+                # v3.2 - 5 Antwoorden
+                # v4.0 - 10 Antwoorden
+                mogelijke_antwoorden = [
+                    # 0-5
+                    f"Je kreeg {currency}{loon} om te dansen in een stripclub.",
+                    f"Je Suger Daddy betaalde je {currency}{loon} voor het werk dat je deed voor hem vorige nacht.",
+                    f"Je bent heel goed met je handen en kreeg {currency}{loon}.",
+                    f"Je hebt {currency}{loon} verdient om naaktfoto's online te verkopen.",
+                    f"Je hebt een Onlyfans gestart en verdiende {currency}{loon} op de eerste dag.",
+                    # 5-10
+                    f"Je neemt iedereen van het lokale voetbal team. Ze betalen je {currency}{loon}.",
+                    f"Je bent goed met deepthroaten en verdiende {currency}{loon} van een klant.",
+                    f"Je hebt een leuke nacht en verdiende {currency}{loon}.",
+                    f"Je was gisteren dronken en eindigde met een trio, je kan er amper iets van herinneren... maar je hebt er {currency}{loon} aan overgehouden.",
+                    f"Je vriend betaalde je {currency}{loon} om zijn voeten te aanbidden."]
                 antwoord = random.choice(mogelijke_antwoorden)
                 color_succes_fail = 0x1bd115
 
@@ -74,32 +111,27 @@ class EcoSlut(commands.Cog):
                 timestamp=datetime.datetime.utcnow()
             )
             em.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
-            em.set_footer(text=embed_footer)
+            em.set_footer(text=footer)
             await ctx.send(embed=em)
-
             db_maxerg.close()
 
     @slut.error
     async def slut_error(self, ctx, error):
         if isinstance(error, commands.CommandOnCooldown):
             cooldown_limit = error.retry_after
-            if cooldown_limit >= 86400:
-                conversion = time.strftime("%#dd %#Hu %#Mm %#Ss", time.gmtime(error.retry_after))
-            elif 3600 <= cooldown_limit < 86400:
+            if cooldown_limit >= 3600:
                 conversion = time.strftime("%#Hu %#Mm %#Ss", time.gmtime(error.retry_after))
             else:
                 conversion = time.strftime("%#Mm %#Ss", time.gmtime(error.retry_after))
 
             em = discord.Embed(
                 description=f"<:error:725030739531268187> Je moet {conversion} wachten om deze command opnieuw te gebruiken.",
-                color=embed_color,
+                color=embedcolor,
                 timestamp=datetime.datetime.utcnow()
             )
             em.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
-            em.set_footer(text=embed_footer)
+            em.set_footer(text=footer)
             await ctx.send(embed=em)
-        else:
-            raise error
 
 
 def setup(client):
