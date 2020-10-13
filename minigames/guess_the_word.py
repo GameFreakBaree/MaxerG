9d1dd9a3a8d2148d21fc54248ef8a0e2b05b82fa
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-13 17:18:26 +0200

    Update guess_the_word.py

--- minigames/guess_the_word.py
+++ minigames/guess_the_word.py
@@ -1,25 +1,38 @@
 import discord
 from discord.ext import commands
-import json
 import mysql.connector
 import asyncio
 import random
+from settings import host, user, password, database, embedcolor, footer
 
-with open('./config.json', 'r', encoding='utf-8') as read_settings:
-    settings = json.load(read_settings)
+t = ["water", "google",
+     "maxerg", "coca-cola",
+     "bloemkool", "limonade",
+     "frieten", "python",
+     "apple", "macaroni",
+     "monitor", "lasagne",
+     "toetsenbord", "gordijn",
+     "muismat", "skateboard",
+     "headset", "rekenmachine",
+     "papier", "discord",
+     "microfoon", "developer",
+     "macbook", "roblox",
+     "iphone", "gemeentehuis",
+     "slaapzak", "politie",
+     "boekentas", "brandweer",
+     "rugzak", "ziekenhuis",
+     "gordijn", "bananen",
+     "airpods", "schoolbanken",
+     "limonade", "nintendo",
+     "playstation", "steelseries",
+     "simulator", "microsoft",
+     "minecraft", "popmuziek",
+     "minetopia", "verwijderen",
+     "nachtkastje", "opslaan",
+     "youtube", "opblazen"]
 
-host = settings['host']
-user = settings['user']
-password = settings['password']
-database = settings['database']
-embedcolor = settings['embedcolor']
-embed_footer = settings['footer']
-read_settings.close()
 
-embed_color = int(embedcolor, 16)
-
-
-class HigherLower(commands.Cog):
+class GuessTheWord(commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -32,46 +45,73 @@ class HigherLower(commands.Cog):
                 db_maxerg = mysql.connector.connect(host=host, database=database, user=user, passwd=password)
                 maxergdb_cursor = db_maxerg.cursor()
 
-                maxergdb_cursor.execute("SELECT random_woord FROM maxerg_guessword")
-                random_word = maxergdb_cursor.fetchone()
+                maxergdb_cursor.execute("SELECT * FROM maxerg_guessword")
+                gtw_data = maxergdb_cursor.fetchone()
+                random_word = gtw_data[0]
+                votes = gtw_data[1]
+                vote1 = gtw_data[2]
+                vote2 = gtw_data[3]
+                vote_list = [vote1, vote2]
 
-                maxergdb_cursor.execute("SELECT last_user_id FROM maxerg_guessword")
-                vorige_id_tuple = maxergdb_cursor.fetchone()
-                vorige_id = int(vorige_id_tuple[0])
+                error_emote = "❌"
+                check_emote = "✅"
 
-                if message.author.id == vorige_id:
-                    await message.channel.purge(limit=1)
-                else:
-                    try:
-                        update_sql = f"UPDATE maxerg_guessword SET last_user_id = 0"
-                        maxergdb_cursor.execute(update_sql)
-                        db_maxerg.commit()
+                if message.content.lower() == random_word:
+                    await message.add_reaction(emoji=check_emote)
 
-                        error_emote = "❌"
-                        check_emote = "✅"
+                    embed = discord.Embed(
+                        title="Woord Geraden!",
+                        description=f"**{message.author.display_name}** heeft het woord geraden!"
+                                    f"\n__Woord:__ ` {random_word} `",
+                        color=embedcolor
+                    )
+                    embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
+                    embed.set_footer(text=footer)
+                    await message.channel.send(embed=embed)
 
-                        if message.content.lower() == random_word[0]:
-                            await message.add_reaction(emoji=check_emote)
+                    random_nieuw_woord = random.choice(t)
+                    maxergdb_cursor.execute("UPDATE maxerg_guessword SET random_woord = %s", [random_nieuw_woord])
+                    maxergdb_cursor.execute("UPDATE maxerg_guessword SET votes = 0")
+                    maxergdb_cursor.execute("UPDATE maxerg_guessword SET vote_one = 0")
+                    maxergdb_cursor.execute("UPDATE maxerg_guessword SET vote_two = 0")
+                    db_maxerg.commit()
 
+                    async with message.channel.typing():
+                        await asyncio.sleep(3)
+
+                    nieuw_woord_shuffle = list(random_nieuw_woord)
+                    random.shuffle(nieuw_woord_shuffle)
+
+                    embed = discord.Embed(
+                        title="Raad Het Woord",
+                        description=f"Het volgende woord is: ` " + "".join(nieuw_woord_shuffle) + " `",
+                        color=embedcolor
+                    )
+                    embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
+                    embed.set_footer(text=f"{footer} | Type '?' om dit woord over te slaan.")
+                    await message.channel.send(embed=embed)
+                elif message.content.lower() == "?":
+                    if votes == 2:
+                        if message.author.id not in vote_list:
                             embed = discord.Embed(
-                                title="Woord Geraden!",
-                                description=f"**{message.author.display_name}** heeft het woord geraden!"
-                                            f"\n__Woord:__ ` {random_word[0]} `",
-                                color=embed_color
+                                title="Woord Gereset!",
+                                description=f"**Niemand** heeft het woord geraden!"
+                                            f"\n__Woord:__ ` {random_word} `",
+                                color=embedcolor
                             )
                             embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
-                            embed.set_footer(text=embed_footer)
+                            embed.set_footer(text=f"{footer}")
                             await message.channel.send(embed=embed)
 
-                            t = []
-
                             random_nieuw_woord = random.choice(t)
-
-                            update_sql2 = f"UPDATE maxerg_guessword SET random_woord = %s"
-                            maxergdb_cursor.execute(update_sql2, (random_nieuw_woord,))
+                            maxergdb_cursor.execute("UPDATE maxerg_guessword SET random_woord = %s", [random_nieuw_woord])
+                            maxergdb_cursor.execute("UPDATE maxerg_guessword SET votes = 0")
+                            maxergdb_cursor.execute("UPDATE maxerg_guessword SET vote_one = 0")
+                            maxergdb_cursor.execute("UPDATE maxerg_guessword SET vote_two = 0")
                             db_maxerg.commit()
 
-                            await asyncio.sleep(3)
+                            async with message.channel.typing():
+                                await asyncio.sleep(3)
 
                             nieuw_woord_shuffle = list(random_nieuw_woord)
                             random.shuffle(nieuw_woord_shuffle)
@@ -79,17 +119,38 @@ class HigherLower(commands.Cog):
                             embed = discord.Embed(
                                 title="Raad Het Woord",
                                 description=f"Het volgende woord is: ` " + "".join(nieuw_woord_shuffle) + " `",
-                                color=embed_color
+                                color=embedcolor
                             )
                             embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
-                            embed.set_footer(text=embed_footer)
+                            embed.set_footer(text=f"{footer} | Type '?' om dit woord over te slaan.")
                             await message.channel.send(embed=embed)
-                        else:
-                            await message.add_reaction(emoji=error_emote)
-                    except ValueError:
-                        await message.channel.purge(limit=1)
+                    else:
+                        if message.author.id not in vote_list:
+                            if 2-votes == 1:
+                                woord = "vote"
+                            else:
+                                woord = "votes"
+
+                            embed = discord.Embed(
+                                title="Raad Het Woord",
+                                description=f"Bedankt voor het voten! Nog {2 - votes} {woord} nodig.",
+                                color=embedcolor
+                            )
+                            embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
+                            embed.set_footer(text=f"{footer} | Type '?' om dit woord over te slaan.")
+                            await message.channel.send(embed=embed)
+
+                            if votes+1 == 1:
+                                maxergdb_cursor.execute(f"UPDATE maxerg_guessword SET vote_one = {message.author.id}")
+                            elif votes+1 == 2:
+                                maxergdb_cursor.execute(f"UPDATE maxerg_guessword SET vote_two = {message.author.id}")
+
+                            maxergdb_cursor.execute("UPDATE maxerg_guessword SET votes = votes + 1")
+                            db_maxerg.commit()
+                else:
+                    await message.add_reaction(emoji=error_emote)
                 db_maxerg.close()
 
 
 def setup(client):
-    client.add_cog(HigherLower(client))
+    client.add_cog(GuessTheWord(client))
