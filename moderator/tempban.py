3e0212f2f066296b8594737931c2ad1751f5b8b8
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-13 17:15:38 +0200

    Update tempban.py

--- moderator/tempban.py
+++ moderator/tempban.py
@@ -1,21 +1,8 @@
 import discord
 import datetime
-import json
 from discord.ext import commands
 import asyncio
-from discord.ext.commands import MissingPermissions
-
-with open('./config.json', 'r', encoding='utf-8') as read_settings:
-    settings = json.load(read_settings)
-
-host = settings['host']
-user = settings['user']
-password = settings['password']
-database = settings['database']
-embedcolor = settings['embedcolor']
-embed_footer = settings['footer']
-read_settings.close()
-embed_color = int(embedcolor, 16)
+from settings import footer, embedcolor
 
 
 class Tempban(commands.Cog):
@@ -42,7 +29,7 @@ class Tempban(commands.Cog):
 
             pretempban_embed = discord.Embed(
                 description=f"**Reden:** {reason}\n**Duratie:** {days}d {hours}u {minutes}m",
-                color=embed_color
+                color=embedcolor
             )
             pretempban_embed.set_image(
                 url="https://media1.tenor.com/images/de413d89fff5502df7cff9f68b24dca5/tenor.gif?itemid=12850590")
@@ -51,7 +38,7 @@ class Tempban(commands.Cog):
 
             log_channel = self.client.get_channel(561243076450975754)
             ban_embed = discord.Embed(
-                color=embed_color,
+                color=embedcolor,
                 timestamp=datetime.datetime.utcnow()
             )
             ban_embed.add_field(name="Gebruiker", value=f"**Naam:**\t{member.mention}\n**ID:**\t{member.id}",
@@ -61,7 +48,7 @@ class Tempban(commands.Cog):
             ban_embed.add_field(name="Reden", value=f"{reason}", inline=False)
             ban_embed.add_field(name="Duur", value=f"{days}d {hours}u {minutes}m", inline=False)
             ban_embed.set_author(name=f"[TEMPBAN] {member}", icon_url=member.avatar_url)
-            ban_embed.set_footer(text=embed_footer)
+            ban_embed.set_footer(text=footer)
             await log_channel.send(embed=ban_embed)
 
             await asyncio.sleep(duration)
@@ -70,7 +57,7 @@ class Tempban(commands.Cog):
 
             log_channel = self.client.get_channel(561243076450975754)
             unban_embed = discord.Embed(
-                color=embed_color,
+                color=embedcolor,
                 timestamp=datetime.datetime.utcnow()
             )
             unban_embed.add_field(name="Gebruiker", value=f"**Naam:**\t{member}",
@@ -79,13 +66,13 @@ class Tempban(commands.Cog):
                                   value=f"**Naam:**\t{ctx.author.mention}\n**ID:**\t{ctx.author.id}",
                                   inline=True)
             unban_embed.set_author(name=f"[UNBAN] {member}", icon_url=ctx.author.default_avatar_url)
-            unban_embed.set_footer(text=embed_footer)
+            unban_embed.set_footer(text=footer)
             await log_channel.send(embed=unban_embed)
 
     @tempban.error
     async def tempban_error(self, ctx, error):
-        if isinstance(error, MissingPermissions):
-            return
+        if isinstance(error, commands.MissingPermissions):
+            pass
 
 
 def setup(client):
