f3a3ba498af98c072741276a4c67176d1b7f3b8e
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-13 17:19:18 +0200

    Update never_have_i_ever.py

--- minigames/never_have_i_ever.py
+++ minigames/never_have_i_ever.py
@@ -1,19 +1,7 @@
 import discord
 from discord.ext import commands
 import random
-import json
-
-with open('./config.json', 'r', encoding='utf-8') as read_settings:
-    settings = json.load(read_settings)
-
-host = settings['host']
-user = settings['user']
-password = settings['password']
-database = settings['database']
-embedcolor = settings['embedcolor']
-embed_footer = settings['footer']
-read_settings.close()
-embed_color = int(embedcolor, 16)
+from settings import embedcolor, footer, minigame_channels
 
 
 class NeverHaveIEver(commands.Cog):
@@ -23,8 +11,7 @@ class NeverHaveIEver(commands.Cog):
 
     @commands.command(aliases=["nhie", "never-have-i-ever", "ik-heb-nog-nooit", "ikhebnognooit", "ihnn"])
     async def neverhaveiever(self, ctx):
-        command_channels = ["🎨│minigames", "🔒│bots"]
-        if str(ctx.channel) in command_channels:
+        if str(ctx.channel) in minigame_channels:
             t = ["Ik ben nog nooit in slaap gevallen in de cinema",
                  "Ik heb nog nooit een vals ID gebruikt om ergens binnen te komen",
                  "Ik heb nog nooit iets gewonnen",
@@ -53,10 +40,10 @@ class NeverHaveIEver(commands.Cog):
             responses = random.choice(t)
             embed = discord.Embed(
                 description=f"{responses}.\n\n1️⃣ I Have\n2️⃣ I Have Not",
-                color=embed_color
+                color=embedcolor
             )
             embed.set_author(name="Never Have I Ever", icon_url=self.client.user.avatar_url)
-            embed.set_footer(text=embed_footer)
+            embed.set_footer(text=footer)
             msg = await ctx.send(embed=embed)
             for emoji in ('1️⃣', '2️⃣'):
                 await msg.add_reaction(emoji=f"{emoji}")
