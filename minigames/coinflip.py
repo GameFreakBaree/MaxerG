5daed8a9692670f188a60c62a805c039961b667c
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-13 17:17:49 +0200

    Update coinflip.py

--- minigames/coinflip.py
+++ minigames/coinflip.py
@@ -1,21 +1,8 @@
 import discord
 from discord.ext import commands
 import datetime
-import json
 from random import randint
-
-with open('./config.json', 'r', encoding='utf-8') as read_settings:
-    settings = json.load(read_settings)
-
-host = settings['host']
-user = settings['user']
-password = settings['password']
-database = settings['database']
-embedcolor = settings['embedcolor']
-embed_footer = settings['footer']
-read_settings.close()
-
-embed_color = int(embedcolor, 16)
+from settings import embedcolor, footer, minigame_channels
 
 
 class Coinflip(commands.Cog):
@@ -25,8 +12,7 @@ class Coinflip(commands.Cog):
 
     @commands.command()
     async def coinflip(self, ctx):
-        command_channels = ["🎨│minigames", "🔒│bots"]
-        if str(ctx.channel) in command_channels:
+        if str(ctx.channel) in minigame_channels:
             t = ["kop", "munt"]
             responses = t[randint(0, 1)]
 
@@ -38,12 +24,12 @@ class Coinflip(commands.Cog):
             embed = discord.Embed(
                 title="Coinflip",
                 description=f"Ik heb een muntstuk gegooid en het muntstuk is gevallen op {responses}!",
-                color=embed_color,
+                color=embedcolor,
                 timestamp=datetime.datetime.utcnow()
             )
             embed.set_thumbnail(url=f"{euro}")
             embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
-            embed.set_footer(text=embed_footer)
+            embed.set_footer(text=footer)
             await ctx.send(embed=embed)
 
 
