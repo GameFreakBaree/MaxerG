3d07c858a98f9d3021056873ca121ced48dfcadd
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-13 17:23:02 +0200

    Update leveling.py

--- commands/leveling.py
+++ commands/leveling.py
@@ -1,22 +1,10 @@
 import discord
 from discord.ext import commands
-import json
 from random import randint
 from discord.utils import get
 import datetime
 import mysql.connector
-
-with open('./config.json', 'r', encoding='utf-8') as read_settings:
-    settings = json.load(read_settings)
-
-host = settings['host']
-user = settings['user']
-password = settings['password']
-database = settings['database']
-embedcolor = settings['embedcolor']
-embed_footer = settings['footer']
-read_settings.close()
-embed_color = int(embedcolor, 16)
+from settings import host, user, password, database, embedcolor, footer, command_channels
 
 
 class LevelingSystem(commands.Cog):
@@ -31,43 +19,20 @@ class LevelingSystem(commands.Cog):
             bucket = self.cd_mapping.get_bucket(message)
             retry_after = bucket.update_rate_limit()
             if retry_after:
-                return
+                pass
             else:
                 db_maxerg = mysql.connector.connect(host=host, database=database, user=user, passwd=password)
                 maxergdb_cursor = db_maxerg.cursor()
-
-                # dead_channels = ["📈│memes"]
-                # if str(message.channel) in dead_channels:
-                #     random_exp = randint(6, 14)
-                # else:
-                random_exp = randint(6, 14)
+                random_exp = randint(3, 7)
 
                 maxergdb_cursor.execute(f"SELECT user_id FROM maxerg_levels WHERE user_id = {message.author.id}")
                 user_id = maxergdb_cursor.fetchone()
 
                 if user_id is None:
-                    with open('./old_data/levels.json', 'r') as f:
-                        user_level_data = json.load(f)
-
-                    if f'{message.author.id}' in user_level_data:
-                        experience = user_level_data[f'{message.author.id}']['experience']
-                        messages = user_level_data[f'{message.author.id}']['berichten']
-                        lvl_start = user_level_data[f'{message.author.id}']['level']
-
-                        if messages == 0:
-                            messages = 1
-                    else:
-                        experience = 0
-                        messages = 1
-                        lvl_start = 0
-
-                    exp_new_random = experience + random_exp
-
                     instert_new_user_id = "INSERT INTO maxerg_levels (user_id, experience, berichten, level) VALUES (%s, %s, %s, %s)"
-                    lvl_record = (message.author.id, exp_new_random, messages, lvl_start)
+                    lvl_record = (message.author.id, random_exp, 1, 0)
                     maxergdb_cursor.execute(instert_new_user_id, lvl_record)
                     db_maxerg.commit()
-
                     experience = random_exp
                 else:
                     maxergdb_cursor.execute(f"SELECT experience FROM maxerg_levels WHERE user_id = {message.author.id}")
@@ -83,72 +48,52 @@ class LevelingSystem(commands.Cog):
                 lvl_start = lvl_start_tuple[0]
 
                 level_up_channel = self.client.get_channel(563347368037056513)
-                guild = message.guild
                 lvl_end = int(experience ** (1 / 3.5))
                 if lvl_start < lvl_end:
                     if lvl_end >= 5:
-                        lordrole = get(guild.roles, name='LvL.5')
-                        await message.author.add_roles(lordrole)
+                        role = get(message.guild.roles, name='LvL.5')
+                        await message.author.add_roles(role)
                     if lvl_end >= 10:
-                        titanrole = get(guild.roles, name='LvL.10')
-                        await message.author.add_roles(titanrole)
+                        role = get(message.guild.roles, name='LvL.10')
+                        await message.author.add_roles(role)
                     if lvl_end >= 15:
-                        godrole = get(guild.roles, name='LvL.15')
-                        await message.author.add_roles(godrole)
+                        role = get(message.guild.roles, name='LvL.15')
+                        await message.author.add_roles(role)
                     if lvl_end >= 20:
-                        overlordrole = get(guild.roles, name='LvL.20')
-                        await message.author.add_roles(overlordrole)
+                        role = get(message.guild.roles, name='LvL.20')
+                        await message.author.add_roles(role)
 
                     level_up_embed = discord.Embed(
                         title=f"{message.author.display_name}",
                         description=f"{message.author.mention} is nu level **{lvl_end}**!",
-                        color=embed_color,
+                        color=embedcolor,
                         timestamp=datetime.datetime.utcnow()
                     )
                     level_up_embed.set_thumbnail(url=message.author.avatar_url)
                     level_up_embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
-                    level_up_embed.set_footer(text=embed_footer)
+                    level_up_embed.set_footer(text=footer)
                     await level_up_channel.send(embed=level_up_embed)
 
-                    level_sql_lvl = f"UPDATE maxerg_levels SET level = {lvl_end} WHERE user_id = {message.author.id}"
-                    maxergdb_cursor.execute(level_sql_lvl)
+                    maxergdb_cursor.execute(f"UPDATE maxerg_levels SET level = {lvl_end} WHERE user_id = {message.author.id}")
                     db_maxerg.commit()
 
-                random_money = randint(2, 8)
-
-                maxergdb_cursor.execute(f"SELECT user_id FROM maxerg_ecogame WHERE user_id = {message.author.id}")
+                random_money = randint(4, 12)
+                maxergdb_cursor.execute(f"SELECT user_id FROM maxerg_economie WHERE user_id = {message.author.id}")
                 user_id_eco = maxergdb_cursor.fetchone()
 
                 if user_id_eco is None:
-                    with open('./old_data/ecogame.json', 'r') as f:
-                        user_eco_data = json.load(f)
-
-                    if f'{message.author.id}' in user_eco_data:
-                        cash = user_eco_data[f'{message.author.id}']['cash']
-                        bank = user_eco_data[f'{message.author.id}']['bank']
-                        netto = cash + bank
-                    else:
-                        cash = 0
-                        bank = 0
-                        netto = 0
-
-                    cash_new_random = cash + random_money
-                    netto_new_random = netto + random_money
-
-                    instert_new_user_id_eco = "INSERT INTO maxerg_ecogame (user_id, cash, bank, netto) VALUES (%s, %s, %s, %s)"
-                    ecogame_record = (message.author.id, cash_new_random, bank, netto_new_random)
-                    maxergdb_cursor.execute(instert_new_user_id_eco, ecogame_record)
+                    insert_new_user_id_eco = "INSERT INTO maxerg_economie (user_id, cash, bank, netto, prestige, job, last_work, risico, max_bank) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                    ecogame_record = (message.author.id, 0, 0, 0, 0, "werkloos", "2020-09-25 01:01:01", 0, 5000)
+                    maxergdb_cursor.execute(insert_new_user_id_eco, ecogame_record)
                     db_maxerg.commit()
                 else:
-                    maxergdb_cursor.execute(f"UPDATE maxerg_ecogame SET cash = cash + {random_money} WHERE user_id = {message.author.id}")
-                    maxergdb_cursor.execute(f"UPDATE maxerg_ecogame SET netto = netto + {random_money} WHERE user_id = {message.author.id}")
+                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET cash = cash + {random_money} WHERE user_id = {message.author.id}")
+                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET netto = netto + {random_money} WHERE user_id = {message.author.id}")
                     db_maxerg.commit()
-
                 db_maxerg.close()
 
     @commands.command(aliases=["level"])
     async def rank(self, ctx, *, member: discord.Member = None):
-        command_channels = ["🤖│commands", "🔒│bots"]
         if str(ctx.channel) in command_channels:
             if member is None:
                 member = ctx.author
@@ -156,35 +101,50 @@ class LevelingSystem(commands.Cog):
             db_maxerg = mysql.connector.connect(host=host, database=database, user=user, passwd=password)
             maxergdb_cursor = db_maxerg.cursor()
 
-            maxergdb_cursor.execute(f"SELECT berichten FROM maxerg_levels WHERE user_id = {member.id}")
-            berichten = maxergdb_cursor.fetchone()
+            maxergdb_cursor.execute(f"SELECT * FROM maxerg_levels WHERE user_id = {member.id}")
+            lvl_data = maxergdb_cursor.fetchone()
+            level = lvl_data[3]
+            berichten = lvl_data[2]
+            exp = lvl_data[1]
+            db_maxerg.close()
 
-            maxergdb_cursor.execute(f"SELECT level FROM maxerg_levels WHERE user_id = {member.id}")
-            level_check = maxergdb_cursor.fetchone()
+            voortgang = round(exp ** (1 / 3.5), 1)
+            oude_voortgang = str(voortgang-int(voortgang)).split('.')[1]
+            oude_voortgang = oude_voortgang[0]
+            voortgang = int(oude_voortgang) * 2
+
+            voortgang_bar = "❚" * voortgang
+            lengte_bar = len(voortgang_bar)
+            rest = 20-lengte_bar
+            rest_bar = "❘" * rest
+
+            procent_voortgang = round(exp ** (1 / 3.5), 2)
+            procent_voortgang = str(procent_voortgang-int(procent_voortgang)).split('.')[1]
+            procent = procent_voortgang[0] + procent_voortgang[1]
+
+            voortgang = f"{voortgang_bar}{rest_bar} ➼ {procent}%"
 
             level_embed = discord.Embed(
                 title=f"{member.display_name}",
-                color=embed_color,
+                description=f"**Voortgang**\n{voortgang}",
+                color=embedcolor,
                 timestamp=datetime.datetime.utcnow()
             )
-            level_embed.add_field(name="Level", value=f"{level_check[0]}", inline=True)
-            level_embed.add_field(name="Berichten", value=f"{berichten[0]}", inline=True)
+            level_embed.add_field(name="Level", value=f"{level}", inline=True)
+            level_embed.add_field(name="Berichten", value=f"{berichten}", inline=True)
             level_embed.set_thumbnail(url=member.avatar_url)
             level_embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
-            level_embed.set_footer(text=embed_footer)
+            level_embed.set_footer(text=footer)
             await ctx.send(embed=level_embed)
-            db_maxerg.close()
 
     @commands.command()
     async def levels(self, ctx, page=1):
-        command_channels = ["🤖│commands", "🔒│bots", "🔒│staff"]
         if str(ctx.channel) in command_channels:
             db_maxerg = mysql.connector.connect(host=host, database=database, user=user, passwd=password)
             maxergdb_cursor = db_maxerg.cursor()
 
             pre_offset = page - 1
             offset = pre_offset * 10
-
             after_str = ""
             eerste_volgnummer = offset
 
@@ -220,11 +180,11 @@ class LevelingSystem(commands.Cog):
             embed = discord.Embed(
                 title=f"Leaderboard [Pagina {page}]",
                 description=f"__Totaal Berichten:__ {alle_berichten}\n\n{after_str}",
-                color=embed_color,
+                color=embedcolor,
                 timestamp=datetime.datetime.utcnow()
             )
             embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
-            embed.set_footer(text=embed_footer)
+            embed.set_footer(text=footer)
             await ctx.send(embed=embed)
             db_maxerg.close()
 
