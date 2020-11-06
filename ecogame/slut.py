b1565da3133a55624790e2db8dd45d2821c90016
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-11-06 21:44:38 +0100

    optimized code

--- ecogame/slut.py
+++ ecogame/slut.py
@@ -5,7 +5,7 @@ from random import randint
 import time
 import datetime
 import mysql.connector
-from settings import host, user, password, database, embedcolor, footer, currency, ecogame_channels
+from settings import host, user, password, database, footer, currency, ecogame_channels, errorcolor
 
 
 class EcoSlut(commands.Cog):
@@ -14,50 +14,20 @@ class EcoSlut(commands.Cog):
         self.client = client
 
     @commands.command()
-    @commands.cooldown(1, 10800, commands.BucketType.user)
+    @commands.cooldown(1, 7200, commands.BucketType.user)
     async def slut(self, ctx):
         if str(ctx.channel) in ecogame_channels:
             db_maxerg = mysql.connector.connect(host=host, database=database, user=user, passwd=password)
             maxergdb_cursor = db_maxerg.cursor()
-            
-            failrate = randint(1, 5)
 
+            failrate = randint(1, 5)
             if failrate == 3:
                 loon = randint(120, 250)
                 loon = int(loon)
 
-                maxergdb_cursor.execute(f"UPDATE maxerg_economie SET cash = cash - {loon} WHERE user_id = {ctx.author.id}")
-                maxergdb_cursor.execute(f"UPDATE maxerg_economie SET netto = netto - {loon} WHERE user_id = {ctx.author.id}")
+                maxergdb_cursor.execute(f"UPDATE maxerg_economie SET cash = cash - {loon}, netto = netto - {loon} WHERE user_id = {ctx.author.id}")
                 db_maxerg.commit()
 
-                risico_new = randint(1, 8)
-                maxergdb_cursor.execute(f"SELECT risico FROM maxerg_economie WHERE user_id = {ctx.author.id}")
-                eco_data = maxergdb_cursor.fetchone()
-                risico = eco_data[0] + risico_new
-
-                risico_waarde = randint(1, 100)
-                if risico_waarde <= risico or risico >= 91:
-                    current_time = datetime.datetime.utcnow()
-                    last_work = current_time.strftime("%Y-%m-%d %H:%M:%S")
-                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET risico = %s WHERE user_id = %s", (104, ctx.author.id))
-                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET job = %s WHERE user_id = %s", ("werkloos", ctx.author.id))
-                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET last_work = %s WHERE user_id = %s", (last_work, ctx.author.id))
-                    db_maxerg.commit()
-
-                    em = discord.Embed(
-                        description=f"Je nam een risico om dit te doen met je job. Je bent Ontslagen!\nJe hebt een cooldown van `7 dagen` om terug te solliciteren.",
-                        color=embedcolor,
-                        timestamp=datetime.datetime.utcnow()
-                    )
-                    em.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
-                    em.set_footer(text=footer)
-                    await ctx.send(embed=em)
-                else:
-                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET risico = {risico} WHERE user_id = {ctx.author.id}")
-                    db_maxerg.commit()
-
-                # v3.2 - 5 Antwoorden
-                # v4.0 - 15 Antwoorden
                 mogelijke_antwoorden = [
                     # 0-5
                     f"De politie viel je huis binnen en je kreeg een boete van {currency}{loon}.",
@@ -71,7 +41,7 @@ class EcoSlut(commands.Cog):
                     f"Je probeerde een sexy standje maar je slipte en brak je been, je betaalde {currency}{loon} aan het ziekenhuis.",
                     f"Terwijl je aan het werken was op de straat voelde je jezelf niet goed. Je ging naar de dokter en hij rekende je {currency}{loon} aan.",
                     f"Je bent betrapt terwijl je seks had in iemand zijn auto en betaalde {currency}{loon} aan de eigenaar van die auto.",
-                    # 11 - 15
+                    # 11-15
                     f"Je gebruikte protectie maar het veroorzaakte een allergische reactie en betaalde {currency}{loon} aan de dokter.",
                     f"Je ging naar beneden bij een boekhouder, de kosten van je belastingen zijn {currency}{loon} verhoogd.",
                     f"Hij wou niet omhoog blijven en je verloor {currency}{loon} aan een klant.",
@@ -80,15 +50,12 @@ class EcoSlut(commands.Cog):
                 antwoord = random.choice(mogelijke_antwoorden)
                 color_succes_fail = 0xFF0000
             else:
-                loon = randint(180, 380)
+                loon = randint(180, 500)
                 loon = int(loon)
 
-                maxergdb_cursor.execute(f"UPDATE maxerg_economie SET cash = cash + {loon} WHERE user_id = {ctx.author.id}")
-                maxergdb_cursor.execute(f"UPDATE maxerg_economie SET netto = netto + {loon} WHERE user_id = {ctx.author.id}")
+                maxergdb_cursor.execute(f"UPDATE maxerg_economie SET cash = cash + {loon}, netto = netto + {loon} WHERE user_id = {ctx.author.id}")
                 db_maxerg.commit()
 
-                # v3.2 - 5 Antwoorden
-                # v4.0 - 10 Antwoorden
                 mogelijke_antwoorden = [
                     # 0-5
                     f"Je kreeg {currency}{loon} om te dansen in een stripclub.",
@@ -114,19 +81,22 @@ class EcoSlut(commands.Cog):
             em.set_footer(text=footer)
             await ctx.send(embed=em)
             db_maxerg.close()
+        else:
+            await ctx.send("Deze command werkt alleen in <#708055327958106164>.")
+            ctx.command.reset_cooldown(ctx)
 
     @slut.error
     async def slut_error(self, ctx, error):
         if isinstance(error, commands.CommandOnCooldown):
             cooldown_limit = error.retry_after
             if cooldown_limit >= 3600:
-                conversion = time.strftime("%#Hu %#Mm %#Ss", time.gmtime(error.retry_after))
+                conversion = time.strftime("%-Hu %-Mm %-Ss", time.gmtime(error.retry_after))
             else:
-                conversion = time.strftime("%#Mm %#Ss", time.gmtime(error.retry_after))
+                conversion = time.strftime("%-Mm %-Ss", time.gmtime(error.retry_after))
 
             em = discord.Embed(
                 description=f"<:error:725030739531268187> Je moet {conversion} wachten om deze command opnieuw te gebruiken.",
-                color=embedcolor,
+                color=errorcolor,
                 timestamp=datetime.datetime.utcnow()
             )
             em.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
