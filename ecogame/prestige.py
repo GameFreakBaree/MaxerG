eb024cc4d7b858daeea34253973c7f812e2e96a6
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-11-06 21:40:42 +0100

    added extra prestiges

--- ecogame/prestige.py
+++ ecogame/prestige.py
@@ -11,7 +11,6 @@ class EcoPrestige(commands.Cog):
         self.client = client
 
     @commands.command()
-    @commands.cooldown(1, 15, commands.BucketType.user)
     async def prestige(self, ctx, info=None):
         if str(ctx.channel) in ecogame_channels:
             if info is None:
@@ -27,40 +26,49 @@ class EcoPrestige(commands.Cog):
                 if prestige == 0 and cash >= 8000:
                     bedrag = 8000
                     max_bank = 10000
-                elif prestige == 1 and cash >= 20000:
-                    bedrag = 20000
+                elif prestige == 1 and cash >= 12000:
+                    bedrag = 12000
                     max_bank = 20000
-                elif prestige == 2 and cash >= 35000:
+                elif prestige == 2 and cash >= 18000:
+                    bedrag = 18000
+                    max_bank = 35000
+                elif prestige == 3 and cash >= 26000:
+                    bedrag = 26000
+                    max_bank = 60000
+                elif prestige == 4 and cash >= 35000:
                     bedrag = 35000
-                    max_bank = 30000
-                elif prestige == 3 and cash >= 50000:
-                    bedrag = 50000
-                    max_bank = 50000
-                elif prestige == 4 and cash >= 70000:
-                    bedrag = 70000
-                    max_bank = 75000
-                elif prestige == 5 and cash >= 90000:
+                    max_bank = 80000
+                elif prestige == 5 and cash >= 48000:
+                    bedrag = 48000
+                    max_bank = 130000
+                elif prestige == 6 and cash >= 60000:
+                    bedrag = 60000
+                    max_bank = 175000
+                elif prestige == 7 and cash >= 72000:
+                    bedrag = 72000
+                    max_bank = 250000
+                elif prestige == 8 and cash >= 90000:
                     bedrag = 90000
-                    max_bank = 100000
-                elif prestige == 6 and cash >= 110000:
-                    bedrag = 110000
-                    max_bank = 150000
-                elif prestige == 7 and cash >= 150000:
-                    bedrag = 150000
-                    max_bank = 200000
-                elif prestige == 8:
+                    max_bank = 300000
+                elif prestige == 9 and cash >= 100000:
+                    bedrag = 100000
+                    max_bank = 360000
+                elif prestige == 10 and cash >= 112000:
+                    bedrag = 112000
+                    max_bank = 420000
+                elif prestige == 11 and cash >= 125000:
+                    bedrag = 125000
+                    max_bank = 500000
+                elif prestige == 12:
                     await ctx.send(f"Je bent al maximum prestige!")
                     doorgaan = False
                 else:
                     await ctx.send(f"Je hebt niet genoeg geld om te prestigen.\nDoe `{prefix}prestige info` om te zien hoeveel het kost.")
                     doorgaan = False
-                    ctx.command.reset_cooldown(ctx)
 
                 if doorgaan:
-                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET prestige = prestige+1 WHERE user_id = {ctx.author.id}")
-                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET cash = cash - {bedrag} WHERE user_id = {ctx.author.id}")
-                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET netto = netto - {bedrag} WHERE user_id = {ctx.author.id}")
-                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET max_bank = {max_bank} WHERE user_id = {ctx.author.id}")
+                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET prestige = prestige+1, max_bank = {max_bank} WHERE user_id = {ctx.author.id}")
+                    maxergdb_cursor.execute(f"UPDATE maxerg_economie SET cash = cash - {bedrag}, netto = netto - {bedrag} WHERE user_id = {ctx.author.id}")
                     db_maxerg.commit()
 
                     embed = discord.Embed(
@@ -77,17 +85,21 @@ class EcoPrestige(commands.Cog):
             else:
                 embed = discord.Embed(
                     title=f"Prestige Info",
-                    description=f"**Wat Is Prestigen?**\nDankzij prestigen kan je hogere jobs ontgrendelen. Bij elke prestige kan je ook meer geld opslag in je bank.\n\n"
-                                f"**Welke Voordelen Krijg ik per level?**"
-                                f"\n• __Prestige 0 (standaard)__ ─ __Prijs:__ {currency}0 ─ Bank Opslag: {currency}5.000, Jobs: 2"
-                                f"\n• __Prestige 1__ ─ __Prijs:__ {currency}8.000 ─ Bank Opslag: {currency}10.000, Jobs: 3"
-                                f"\n• __Prestige 2__ ─ __Prijs:__ {currency}20.000 ─ Bank Opslag: {currency}20.000, Jobs: 4"
-                                f"\n• __Prestige 3__ ─ __Prijs:__ {currency}35.000 ─ Bank Opslag: {currency}30.000, Jobs: 5"
-                                f"\n• __Prestige 4__ ─ __Prijs:__ {currency}50.000 ─ Bank Opslag: {currency}50.000, Jobs: 6"
-                                f"\n• __Prestige 5__ ─ __Prijs:__ {currency}70.000 ─ Bank Opslag: {currency}75.000, Jobs: 7"
-                                f"\n• __Prestige 6__ ─ __Prijs:__ {currency}90.000 ─ Bank Opslag: {currency}100.000, Jobs: 8"
-                                f"\n• __Prestige 7__ ─ __Prijs:__ {currency}110.000 ─ Bank Opslag: {currency}150.000, Jobs: 9"
-                                f"\n• __Prestige 8__ ─ __Prijs:__ {currency}150.000 ─ Bank Opslag: {currency}200.000, Jobs: 10",
+                    description=f"**Wat Is Prestigen?**\nDankzij prestigen kan je hogere jobs ontgrendelen. Bij elke prestige krijg je meer opslag in je bank.\n\n"
+                                f"**Welke voordelen krijg ik per level?**"
+                                f"\n• __Prestige 0 (standaard)__ ─ __Prijs:__ {currency}0 ─ Bank Opslag: {currency}5.000, Jobs: 1"
+                                f"\n• __Prestige 1__ ─ __Prijs:__ {currency}8.000 ─ Opslag: {currency}10.000, Jobs: 2"
+                                f"\n• __Prestige 2__ ─ __Prijs:__ {currency}12.000 ─ Opslag: {currency}20.000, Jobs: 3"
+                                f"\n• __Prestige 3__ ─ __Prijs:__ {currency}18.000 ─ Opslag: {currency}35.000, Jobs: 4"
+                                f"\n• __Prestige 4__ ─ __Prijs:__ {currency}26.000 ─ Opslag: {currency}60.000, Jobs: 5"
+                                f"\n• __Prestige 5__ ─ __Prijs:__ {currency}35.000 ─ Opslag: {currency}80.000, Jobs: 6"
+                                f"\n• __Prestige 6__ ─ __Prijs:__ {currency}48.000 ─ Opslag: {currency}130.000, Jobs: 7"
+                                f"\n• __Prestige 7__ ─ __Prijs:__ {currency}60.000 ─ Opslag: {currency}175.000, Jobs: 8"
+                                f"\n• __Prestige 8__ ─ __Prijs:__ {currency}72.000 ─ Opslag: {currency}250.000, Jobs: 9"
+                                f"\n• __Prestige 9__ ─ __Prijs:__ {currency}90.000 ─ Opslag: {currency}300.000, Jobs: 10"
+                                f"\n• __Prestige 10__ ─ __Prijs:__ {currency}100.000 ─ Opslag: {currency}360.000, Jobs: 11"
+                                f"\n• __Prestige 11__ ─ __Prijs:__ {currency}112.000 ─ Opslag: {currency}420.000, Jobs: 12"
+                                f"\n• __Prestige 12__ ─ __Prijs:__ {currency}125.000 ─ Opslag: {currency}500.000, Jobs: 13",
                     color=embedcolor,
                     timestamp=datetime.datetime.utcnow()
                 )
@@ -96,19 +108,6 @@ class EcoPrestige(commands.Cog):
                 await ctx.send(embed=embed)
         else:
             await ctx.send("Deze command werkt alleen in <#708055327958106164>.")
-            ctx.command.reset_cooldown(ctx)
-
-    @prestige.error
-    async def prestige_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            em = discord.Embed(
-                description=f"<:error:725030739531268187> Je moet {round(error.retry_after, 1)} seconden wachten om deze command opnieuw te gebruiken.",
-                color=embedcolor,
-                timestamp=datetime.datetime.utcnow()
-            )
-            em.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
-            em.set_footer(text=footer)
-            await ctx.send(embed=em)
 
 
 def setup(client):
