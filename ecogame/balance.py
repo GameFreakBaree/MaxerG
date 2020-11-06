20ce204a7f5e22e359755e9389853ed41b0787b1
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-11-06 21:34:18 +0100

    removed cooldown + added netto security check

--- ecogame/balance.py
+++ ecogame/balance.py
@@ -11,7 +11,6 @@ class EcoBalance(commands.Cog):
         self.client = client
 
     @commands.command(aliases=["bal"])
-    @commands.cooldown(1, 5, commands.BucketType.user)
     async def balance(self, ctx, *, member: discord.Member = None):
         if str(ctx.channel) in ecogame_channels:
             if member is None:
@@ -24,14 +23,18 @@ class EcoBalance(commands.Cog):
             eco_data = maxergdb_cursor.fetchone()
             cash = eco_data[1]
             bank = eco_data[2]
-            netto = eco_data[3]
-            max_bank = eco_data[8]
+            netto_check = eco_data[3]
+            max_bank = eco_data[6]
 
             if cash is None or bank is None or max_bank is None:
                 cash = 0
                 bank = 0
                 max_bank = 5000
+            netto = cash + bank
 
+            if netto_check != netto:
+                maxergdb_cursor.execute(f"UPDATE maxerg_economie SET netto = {netto} WHERE user_id = {member.id}")
+                db_maxerg.commit()
             db_maxerg.close()
 
             em = discord.Embed(
@@ -46,19 +49,6 @@ class EcoBalance(commands.Cog):
             await ctx.send(embed=em)
         else:
             await ctx.send("Deze command werkt alleen in <#708055327958106164>.")
-            ctx.command.reset_cooldown(ctx)
-
-    @balance.error
-    async def balance_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            em = discord.Embed(
-                description=f"<:error:725030739531268187> Je moet {round(error.retry_after, 1)} seconden wachten om deze command opnieuw te gebruiken.",
-                color=embedcolor,
-                timestamp=datetime.datetime.utcnow()
-            )
-            em.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
-            em.set_footer(text=footer)
-            await ctx.send(embed=em)
 
 
 def setup(client):
