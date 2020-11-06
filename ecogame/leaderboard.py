57ea3189f02eee1987280ed7b24279b5266f7e96
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-11-06 21:40:12 +0100

    removed cooldown + multi page lb with emotes

--- ecogame/leaderboard.py
+++ ecogame/leaderboard.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 import datetime
 import mysql.connector
-from settings import host, user, password, database, embedcolor, footer, ecogame_channels
+import asyncio
+from settings import host, user, password, database, embedcolor, footer, ecogame_channels, currency
 
 
 class EcoLeaderboard(commands.Cog):
@@ -11,22 +12,26 @@ class EcoLeaderboard(commands.Cog):
         self.client = client
 
     @commands.command(aliases=["lb", "top"])
-    @commands.cooldown(1, 20, commands.BucketType.user)
     async def leaderboard(self, ctx, waarde=None, page=1):
         if str(ctx.channel) in ecogame_channels:
             doorgaan = True
+            prefix = currency
             if waarde is None:
                 index_waarde = 3
+                waarde = "netto"
             elif waarde.lower() == "cash":
                 index_waarde = 1
             elif waarde.lower() == "bank":
                 index_waarde = 2
             elif waarde.lower() == "netto":
                 index_waarde = 3
+            elif waarde.lower() == "prestige":
+                index_waarde = 4
+                prefix = "Prestige: "
             else:
+                index_waarde = 3
                 try:
                     page = int(waarde)
-                    index_waarde = 3
                 except ValueError:
                     doorgaan = False
 
@@ -38,7 +43,7 @@ class EcoLeaderboard(commands.Cog):
                 after_str = ""
                 eerste_volgnummer = offset
 
-                maxergdb_cursor.execute(f"SELECT * FROM maxerg_economie ORDER BY netto DESC LIMIT 10 OFFSET {offset}")
+                maxergdb_cursor.execute(f"SELECT * FROM maxerg_economie ORDER BY {waarde.lower()} DESC LIMIT 10 OFFSET {offset}")
                 result = maxergdb_cursor.fetchall()
                 for row in result:
                     eerste_volgnummer = eerste_volgnummer + 1
@@ -52,7 +57,7 @@ class EcoLeaderboard(commands.Cog):
                     geld = row[index_waarde]
 
                     if geld != 0:
-                        pre_str = f"**{eerste_volgnummer}.** {top_names} • **€{geld}**\n"
+                        pre_str = f"**{eerste_volgnummer}.** {top_names} • **{prefix}{geld}**\n"
                         after_str = after_str + pre_str
 
                 if after_str == "":
@@ -64,31 +69,90 @@ class EcoLeaderboard(commands.Cog):
                 for row in cash_gegevens:
                     netto_totaal = netto_totaal + row[0]
 
-                db_maxerg.close()
+                maxergdb_cursor.execute(f"SELECT {waarde.lower()} FROM maxerg_economie WHERE {waarde.lower()} != 0")
+                max_pages_tuple = maxergdb_cursor.fetchall()
+
+                max_pages = len(max_pages_tuple)
+
+                if max_pages % 10 != 0:
+                    max_pages = max_pages // 10 + 1
+                else:
+                    max_pages = max_pages // 10
 
                 embed = discord.Embed(
-                    title=f"Leaderboard [Pagina {page}]",
+                    title=f"Leaderboard [Pagina {page}/{max_pages}]",
                     description=f"__Totaal Geld:__ €{netto_totaal}\n\n{after_str}",
                     color=embedcolor,
                     timestamp=datetime.datetime.utcnow()
                 )
                 embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
                 embed.set_footer(text=footer)
-                await ctx.send(embed=embed)
+                leaderboard_message = await ctx.send(embed=embed)
+
+                await leaderboard_message.add_reaction("◀️")
+                await leaderboard_message.add_reaction("▶️")
+
+                def check(reaction, member):
+                    return member == ctx.author and str(reaction.emoji) in ["◀️", "▶️"]
+
+                while True:
+                    try:
+                        reaction, member = await self.client.wait_for("reaction_add", timeout=30, check=check)
+
+                        if str(reaction.emoji) == "▶️" and page != max_pages:
+                            page += 1
+                            await leaderboard_message.remove_reaction(reaction, member)
+                            nieuwe_pagina = True
+                        elif str(reaction.emoji) == "◀️" and page > 1:
+                            page -= 1
+                            await leaderboard_message.remove_reaction(reaction, member)
+                            nieuwe_pagina = True
+                        else:
+                            await leaderboard_message.remove_reaction(reaction, member)
+                            nieuwe_pagina = False
+
+                        if nieuwe_pagina:
+                            offset = (page - 1) * 10
+                            after_str = ""
+                            eerste_volgnummer = offset
+
+                            maxergdb_cursor.execute(f"SELECT * FROM maxerg_economie ORDER BY {waarde.lower()} DESC LIMIT 10 OFFSET {offset}")
+                            result = maxergdb_cursor.fetchall()
+                            for row in result:
+                                eerste_volgnummer = eerste_volgnummer + 1
+
+                                try:
+                                    top_name = self.client.get_user(row[0])
+                                    top_names = top_name.mention
+                                except AttributeError:
+                                    top_names = row[0]
+
+                                geld = row[index_waarde]
+
+                                if geld != 0:
+                                    pre_str = f"**{eerste_volgnummer}.** {top_names} • **{prefix}{geld}**\n"
+                                    after_str = after_str + pre_str
+
+                            if after_str == "":
+                                after_str = "Geen data gevonden!"
+
+                            embed = discord.Embed(
+                                title=f"Leaderboard [Pagina {page}/{max_pages}]",
+                                description=f"__Totaal Geld:__ €{netto_totaal}\n\n{after_str}",
+                                color=embedcolor,
+                                timestamp=datetime.datetime.utcnow()
+                            )
+                            embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
+                            embed.set_footer(text=footer)
+                            await leaderboard_message.edit(embed=embed)
+                    except asyncio.TimeoutError:
+                        await leaderboard_message.clear_reaction("◀️")
+                        await leaderboard_message.clear_reaction("▶️")
+                        break
+
+                db_maxerg.close()
         else:
             await ctx.send("Deze command werkt alleen in <#708055327958106164>.")
-
-    @leaderboard.error
-    async def leaderboard_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            em = discord.Embed(
-                description=f"<:error:725030739531268187> Je moet {round(error.retry_after, 1)} seconden wachten om deze command opnieuw te gebruiken.",
-                color=embedcolor,
-                timestamp=datetime.datetime.utcnow()
-            )
-            em.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
-            em.set_footer(text=footer)
-            await ctx.send(embed=em)
 
 
 def setup(client):
