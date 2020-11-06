c50a9551cc95c9192028d682c058fff83cbe0a8f
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-11-06 21:32:41 +0100

    removed ip command

--- commands/small_cmds.py
+++ commands/small_cmds.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import json
 from settings import embedcolor, footer, command_channels
 
 
@@ -8,19 +7,6 @@ class SmallCmds(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-
-    @commands.command(aliases=['website', 'versie', 'store', 'dynmap'])
-    async def ip(self, ctx):
-        embed = discord.Embed(
-            title="Handige Informatie",
-            description="__IP:__ **play.MaxerG.net**\n__Versie:__ **1.12.2 - 1.16.3**\n"
-                        "__Website:__ **https://www.maxerg.net/**\n__Shop:__ **https://shop.maxerg.net/**"
-                        "\n__Dynmap:__ **https://dynmap.maxerg.net/**",
-            color=embedcolor
-        )
-        embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
-        embed.set_footer(text=footer)
-        await ctx.send(embed=embed)
 
     @commands.command(aliases=["users"])
     async def members(self, ctx):
