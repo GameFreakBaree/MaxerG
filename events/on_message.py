672cef881029b31d4ccd10938668a67cc671b20b
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-11-06 21:29:24 +0100

    Update and rename emote_react.py to on_message.py

--- events/on_message.py
+++ events/on_message.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+last_user = 0
 
 
 class ReactOnMessages(commands.Cog):
@@ -9,18 +10,24 @@ class ReactOnMessages(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if f"<@!{self.client.user.id}>" in message.content:
-            await message.channel.send(
-                f"De Prefix van deze bot is `!`\nJe kan alle commands zien door `!help`!")
+            await message.channel.send(f"De Prefix van deze bot is `!`\nJe kan alle commands zien door `!help`!")
 
         emoji_channels = ["💡│suggesties"]
         if str(message.channel) in emoji_channels:
             for emoji in ('👍', '👎'):
                 await message.add_reaction(emoji=f"{emoji}")
 
-        emoji_channels = ["❓│polls"]
-        if str(message.channel) in emoji_channels:
-            for emoji in ('1️⃣', '2️⃣', '3️⃣'):
-                await message.add_reaction(emoji=f"{emoji}")
+        gtw_channels = ["🐍│woordslang"]
+        if str(message.channel) in gtw_channels:
+            global last_user
+            if last_user == message.author.id:
+                await message.delete()
+            else:
+                last_user = message.author.id
+
+        role_channels = ["🎀│roles"]
+        if str(message.channel) in role_channels:
+            await message.delete()
 
 
 def setup(client):
