4118c276f9ce83d870af56e779de003fb0e8f762
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-11-24 21:33:00 +0100

    Add files via upload

--- minigames/woordslang.py
+++ minigames/woordslang.py
@@ -1,8 +1,11 @@
 from discord.ext import commands
+import string
+
 last_user = 0
+last_word = ""
 
 
-class WoordSlang(commands.Cog):
+class ReactOnMessages(commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -12,11 +15,24 @@ class WoordSlang(commands.Cog):
         gtw_channels = ["🐍│woordslang"]
         if str(message.channel) in gtw_channels:
             global last_user
+            global last_word
+
             if last_user == message.author.id:
                 await message.delete()
             else:
-                last_user = message.author.id
+                if last_word == "":
+                    last_user = message.author.id
+                    last_word = message.content
+                else:
+                    if message.content[-1] in list(string.ascii_lowercase) or message.content[-1] in list(string.ascii_uppercase):
+                        if last_word[-1] == message.content[0]:
+                            last_user = message.author.id
+                            last_word = message.content
+                        else:
+                            await message.delete()
+                    else:
+                        await message.delete()
 
 
 def setup(client):
-    client.add_cog(WoordSlang(client))
+    client.add_cog(ReactOnMessages(client))
